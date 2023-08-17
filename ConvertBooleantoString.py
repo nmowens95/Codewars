cc9e5117f366ada99c95169bf9b4b47225efa809
Nathan Owens <nmowens95@gmail.com>
2023-08-17 15:47:38 -0500

    Solution to Convert a Boolean to String

---
+++ ConvertBooleantoString.py
@@ -0,0 +1,27 @@
+# Instructions:
+'''
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+
+'''
+
+# Solution:
+
+def boolean_to_string(b):
+    return str(b)
+
+
+# Test case:
+'''
+import codewars_test as test
+from solution import boolean_to_string
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(boolean_to_string(True), "True")
+        test.assert_equals(boolean_to_string(False), "False")
+'''
