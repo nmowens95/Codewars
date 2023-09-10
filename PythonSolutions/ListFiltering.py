d680ec32d1954cadbdc6249e0d5f04e6808c07cc
Nathan Owens <nmowens95@gmail.com>
2023-09-10 16:42:14 -0500

    Added solution to List Filtering

---
+++ PythonSolutions/ListFiltering.py
@@ -0,0 +1,33 @@
+# Instructions:
+'''
+In this kata you will create a function that takes a list of non-negative integers 
+and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+'''
+
+# Answer:
+
+def filter_list(list):
+    new_list = []
+    for item in list:
+        if isinstance(item, int):
+            new_list.append(item)
+    return new_list
+
+'''
+Sample Tests:
+import codewars_test as test
+from solution import filter_list
+
+@test.describe('Sample tests')
+def sample_tests():
+    @test.it('should work for basic examples')
+    def basic_examples():
+        test.assert_equals(filter_list([1, 2, 'a', 'b']), [1, 2], 'For input [1, 2, "a", "b"]')
+        test.assert_equals(filter_list([1, 'a', 'b', 0, 15]), [1, 0, 15], 'Fot input [1, "a", "b", 0, 15]')
+        test.assert_equals(filter_list([1, 2, 'aasf', '1', '123', 123]), [1, 2, 123], 'For input [1, 2, "aasf", "1", "123", 123]')
+'''
