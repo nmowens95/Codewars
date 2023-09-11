6d67c74921454a79815255f1d6b439a059771705
nmowens95 <nmowens95@gmail.com>
2023-09-11 11:51:54 -0500

    Added solution to Find the middle element

---
+++ PythonSolutions/FindTheMiddleElement.py
@@ -0,0 +1,37 @@
+# Instructions:
+'''
+As a part of this Kata, you need to create a function that when provided with a triplet, 
+returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+'''
+
+# Answer:
+
+def gimme(input_array):
+    middle = sorted(input_array)[1]
+    return input_array.index(middle)
+
+
+'''
+Sample Tests:
+import codewars_test as test
+from solution import gimme
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(gimme([2, 3, 1]), 0, 'Finds the index of middle element')
+        test.assert_equals(gimme([5, 10, 14]), 1, 'Finds the index of middle element')
+'''
