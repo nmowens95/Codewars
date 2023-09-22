7bfd91c23ab1bf5e911b3b4346c0a3bcec2d517f
nmowens95 <nmowens95@gmail.com>
2023-09-22 15:36:30 -0500

    Added solution to sum of numbers

---
+++ PythonSolutions/SumOfNumbers.py
@@ -0,0 +1,36 @@
+'''
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+Your function should only return a number, not the explanation about how you get that number.
+'''
+
+# Answer:
+
+def get_sum(a,b):
+    if a == b:
+        return a
+    return a + b
+
+
+'''
+Sample Tests:
+from solution import get_sum
+import codewars_test as test
+
+
+@test.describe('Sum of numbers')
+def desc1():
+    @test.it('Sample tests')
+    def it1():
+        test.assert_equals(get_sum(0,1),1)
+        test.assert_equals(get_sum(0,-1),-1)
+'''
