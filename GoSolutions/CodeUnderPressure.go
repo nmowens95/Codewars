c34c6af3f74ae6db03d78374257771b4a75b6512
nmowens95 <nmowens95@gmail.com>
2023-09-22 16:14:29 -0500

    Added solution to You can't code under pressure

---
+++ GoSolutions/CodeUnderPressure.go
@@ -0,0 +1,30 @@
+// Instructions
+
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// Answer:
+package kata
+
+func DoubleInteger(i int) int {
+  return 2 * i
+}
+
+
+// Sample Tests:
+
+/*
+package kata_test
+
+import (
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+  . "codewarrior/kata"
+)
+
+var _ = Describe("Test Example", func() {
+  It("should work for some examples", func() {
+     Expect(DoubleInteger(1)).To(Equal(2))
+   })
+})
+*/
