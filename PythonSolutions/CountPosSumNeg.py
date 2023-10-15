a8c9e8380485c2e687d8b7f6cfcf73b22408e08f
Nathan Owens <nmowens95@gmail.com>
2023-10-14 23:04:43 -0500

    Added solution to count positives/sum negatives in python

---
+++ PythonSolutions/CountPosSumNeg.py
@@ -0,0 +1,48 @@
+# Title: Count of positives / sum of negatives
+
+
+# Instructions:
+'''
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers 
+and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+'''
+
+# Answer:
+
+def count_positives_sum_negatives(arr):
+    if len(arr) == 0:
+            return [] 
+    
+    counter = 0
+    neg_sum = 0
+
+    for nums in arr:
+        if nums > 0:
+            counter += 1
+        else:
+            neg_sum += nums
+    return [counter, neg_sum]
+
+# Sample Tests:
+'''
+import codewars_test as test
+from solution import count_positives_sum_negatives
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65])
+        test.assert_equals(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]),[8,-50])
+        test.assert_equals(count_positives_sum_negatives([1]),[1,0])
+        test.assert_equals(count_positives_sum_negatives([-1]),[0,-1])
+        test.assert_equals(count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]),[0,0])
+        test.assert_equals(count_positives_sum_negatives([]),[])
+'''
