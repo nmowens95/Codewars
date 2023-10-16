68ea3a3b175b3b3b6af7d027b5e3b5e47257a1a2
nmowens95 <nmowens95@gmail.com>
2023-10-16 14:39:50 -0500

    Added solution to Function 1 - hello world in go

---
+++ GoSolutions/Function1HW.go
@@ -0,0 +1,29 @@
+/* Instructions
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+*/
+
+// Answer:
+package kata // do not remove
+
+func greet() string {
+	return "hello world!"
+}
+
+// Sample Tests:
+
+/*
+package kata
+import (
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+)
+
+var _ = Describe("Example Tests", func() {
+  It("should return string: 'hello world!'", func() {
+    Expect(greet()).To(Equal("hello world!"))
+  })
+})
+*/
