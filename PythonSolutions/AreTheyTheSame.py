9fc3a3d2d9d2648a2e705cec8367f16d54c8a5c6
Nathan Owens <nmowens95@gmail.com>
2023-10-24 21:37:40 -0500

    Added solution to are they the same in python

---
+++ PythonSolutions/AreTheyTheSame.py
@@ -0,0 +1,86 @@
+# Title: Are They the "Same"
+
+# Instructions:
+'''
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+Note for C
+The two arrays have the same size (> 0) given as parameter in function comp.
+
+
+'''
+
+# Answer:
+
+def comp(array1, array2):
+    # Check if either array is None
+    if array1 is None or array2 is None:
+        return False
+
+    # Check if the lengths of the arrays are not equal
+    if len(array1) != len(array2):
+        return False
+
+    # Create a dictionary to store the count of squares of elements from array1
+    count_dict = {}
+
+    # Count the squares of elements in array1 and store them in count_dict
+    for num in array1:
+        square = num * num
+        if square in count_dict:
+            count_dict[square] += 1
+        else:
+            count_dict[square] = 1
+
+    # Check if each square of elements in array2 exists in count_dict
+    # and has the same multiplicity
+    for num in array2:
+        if num not in count_dict or count_dict[num] == 0:
+            return False
+        count_dict[num] -= 1
+
+    return True
+
+# Sample Tests:
+'''
+import codewars_test as test
+from solution import comp
+
+@test.describe("Same")
+def tests():
+    @test.it("Fixed tests")
+    def basics():
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+        test.assert_equals(comp(a1, a2), True)
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*21, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+        test.assert_equals(comp(a1, a2), False)
+        a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+        a2 = [11*11, 121*121, 144*144, 190*190, 161*161, 19*19, 144*144, 19*19]
+        test.assert_equals(comp(a1, a2), False)
+'''
