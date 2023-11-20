6ecc16619e08f7c59fbd1cca2437716cd0dcbdc9
nmowens95 <nmowens95@gmail.com>
2023-11-20 15:41:17 -0600

    Added solution ot messi goals function in go

---
+++ GoSolutions/MessiGoalsFx.go
@@ -0,0 +1,39 @@
+/* Instructions
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+
+// Answer:
+package kata
+
+func Goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals int) int {
+	return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
+// Sample Tests:
+
+/*
+package kata_test
+import (
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+  . "codewarrior/kata"
+)
+var _ = Describe("Sample tests", func() {
+   It("should test that the solution returns the correct value", func() {
+     Expect(Goals(0,0,0)).To(Equal(0))
+     Expect(Goals(43, 10, 5)).To(Equal(58))
+   })
+})
+*/
